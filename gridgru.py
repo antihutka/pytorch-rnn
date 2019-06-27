af74f7d191ccf662d293707adcd48872de75a480
Michal Povinsky <antihutka@gmail.com>
2019-06-27 06:29:48 +0200

    Trying to make autograd slightly less horribly slow

--- gridgru.py
+++ gridgru.py
@@ -72,13 +72,15 @@ class GRIDGRU(torch.nn.Module):
     gatesd_nt.addmm_(x_nt, Wxd[:, :2*D])
     gatesd = gatesd_nt.view(N, T, -1)
     #ht = x.new_zeros(N, T, H)
+    xsplit = x.split(1, 1)
     for t in range(0, T):
       #next_ht = ht[:, t]
       
       #cur_gates = gates[:, t]
       #cur_gates_g = cur_gates[:, :2 * H]
-      cur_gates_g = torch.addmm(self.bias[:2*H].expand(N, -1), x[:, t], Wxt[:, :2*H])
-      hc = torch.addmm(self.bias[2*H:3*H].expand(N, -1), x[:, t], Wxt[:, 2*H:3*H])
+      xt = xsplit[t].view(N, D)
+      cur_gates_g = torch.addmm(self.bias[:2*H].expand(N, -1), xt, Wxt[:, :2*H])
+      hc = torch.addmm(self.bias[2*H:3*H].expand(N, -1), xt, Wxt[:, 2*H:3*H])
       cur_gates_g.addmm_(prev_ht, Whtg).sigmoid_()
       u = cur_gates_g[:, :H]
       r = cur_gates_g[:, H:2*H]
