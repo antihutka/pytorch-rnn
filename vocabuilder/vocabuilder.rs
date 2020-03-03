6dbbbf23ab297d6accacc984168e1effd44fda91
Michal Povinsky <antihutka@gmail.com>
2020-03-03 11:30:43 +0100

    More vocabuilder updates

--- vocabuilder/vocabuilder.rs
+++ vocabuilder/vocabuilder.rs
@@ -14,6 +14,10 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use radix_trie::Trie;
 
+struct VocabuilderOptions {
+	min_merge: usize,
+}
+
 fn get_basic_tokens(input: &Mmap) -> HashSet<u8> {
 	let mut bt = HashSet::new();
 	for x in input.iter() {
@@ -48,15 +52,19 @@ fn sorted_pair_counts(pair_counts: &Vec<Vec<usize>>) -> Vec<(usize, usize, usize
 	return cnts;
 }
 
+fn concat_tokens(tokens: &Vec<Vec<u8>>, idx1: usize, idx2: usize) -> Vec<u8> {
+	return [&tokens[idx1][..], &tokens[idx2][..]].concat();
+}
+
 fn print_pair_counts(tokens: &Vec<Vec<u8>>, sorted_pairs: &Vec<(usize, usize, usize)>) {
 	for (idx1, idx2, cnt) in sorted_pairs[0..25].iter() {
-		let mtoken = [&tokens[*idx1][..], &tokens[*idx2][..]].concat();
+		let mtoken = concat_tokens(tokens, *idx1, *idx2);
 		print!("{:?} -> {},  ", decode_utf8(&mtoken), cnt);
 	}
 	println!();
 }
 
-fn do_bpe(input_data: &[u8], basic_tokens: HashSet<u8>) {
+fn do_bpe(input_data: &[u8], basic_tokens: HashSet<u8>, opts: &VocabuilderOptions) {
 	let mut tokens = Vec::new();
 	for x in basic_tokens {
 		tokens.push(vec![x]);
@@ -82,12 +90,17 @@ fn do_bpe(input_data: &[u8], basic_tokens: HashSet<u8>) {
 		}
 		prev_token = v;
 	}
-	//println!("Token counts: {:?}", token_counts);
 	let sorted_tokens = sorted_token_counts(&token_counts);
 	print_token_counts(&tokens, &sorted_tokens);
-	//println!("Pair counts: {:?}", pair_counts);
 	let sorted_pairs = sorted_pair_counts(&pair_counts);
 	print_pair_counts(&tokens, &sorted_pairs);
+	if sorted_pairs[0].2 >= opts.min_merge {
+		let (t1, t2, tc) = sorted_pairs[0];
+		let merged = concat_tokens(&tokens, t1, t2);
+		println!("Merging top pair {:?} + {:?} -> {:?}", decode_utf8(&tokens[t1]), decode_utf8(&tokens[t2]), decode_utf8(&merged));
+	} else {
+		return;
+	}
 }
 
 fn main() {
@@ -95,8 +108,11 @@ fn main() {
 	let matches = App::new("Vocabuilder")
 		.version("0.0.1")
 		.arg(Arg::with_name("input").short("i").long("input").takes_value(true))
+		.arg(Arg::with_name("min_merge").long("min-merge").takes_value(true))
 		.get_matches();
 	let input_file = matches.value_of("input").unwrap();
+	let min_merge = matches.value_of("min_merge").unwrap_or("1000").parse().unwrap();
+	let vopts = VocabuilderOptions{min_merge};
 	println!("input file {}", input_file);
 	let file = File::open(input_file).unwrap();
 	let mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
@@ -105,5 +121,5 @@ fn main() {
 	println!("scanning basic tokens");
 	let basic_tokens = get_basic_tokens(&mmap);
 	println!("done, {} tokens found", basic_tokens.len());
-	do_bpe(&mmap[0..input_length-1], basic_tokens);
+	do_bpe(&mmap[0..input_length-1], basic_tokens, &vopts);
 }
