e12bb15b97a8cfbda157126d3434a1ba199ea2ba
Michal Povinsky <antihutka@gmail.com>
2023-06-13 04:45:21 +0200

    Use 512-bit vector size for CPU kernels

--- extension/setup.py
+++ extension/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
 setup(name='ptrnn_cpp',
-      ext_modules=[cpp_extension.CppExtension('ptrnn_cpp', ['ptrnn.cpp'])],
+      ext_modules=[cpp_extension.CppExtension('ptrnn_cpp', ['ptrnn.cpp'], extra_compile_args={'cxx':['-O3', '-march=native', '-mprefer-vector-width=512', '-ffast-math', '-fopenmp']}, extra_link_args=['-lgomp'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
