ac013279d2eb4abd6948c8bc1937afcddbb805ef
Michal Povinsky <antihutka@gmail.com>
2025-06-19 10:39:02 +0200

    Fix param groups so they actually work

--- train.py
+++ train.py
@@ -82,7 +82,7 @@ for pname, param in model.named_parameters():
   if pname.endswith('.bias'): group = 1
   pgroups[group]['params'].append(param)
   print("Added param %s:%s to group %d" % (pname, str(param.size()), group))
-optimizer = BetterAdamW(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay, grad_clip=args.grad_clip)
+optimizer = BetterAdamW(pgroups, lr=args.learning_rate, weight_decay=args.weight_decay, grad_clip=args.grad_clip)
 scheduler = optim.lr_scheduler.StepLR(optimizer, args.lrdecay_every, args.lrdecay_factor)
 scheduler_start = optim.lr_scheduler.LinearLR(optimizer, start_factor=.1, total_iters=args.warmup_iters)
 crit = nn.CrossEntropyLoss(reduction = 'none' if args.use_masks else 'mean')
